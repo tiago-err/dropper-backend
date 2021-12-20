fc303a300d7cc61067b87e51ea9de19e18b37d0e
Tiago Ribeiro <tiago.err@outlook.com>
2021-12-20 22:13:53 +0000

    Added a log to know when the server was last started

--- server.ts
+++ server.ts
@@ -10,4 +10,5 @@ app.set("supabase", supabase);
 console.log(`Node environment: ${process.env.NODE_ENV}`);
 app.listen(port, () => {
 	console.log(`Example app listening at port http://localhost:${port}`);
+	console.log(`Init Time: ${new Date().toISOString()}`);
 });
